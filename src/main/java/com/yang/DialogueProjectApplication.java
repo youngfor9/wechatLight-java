5b2ecf930516dd1d72dbdec8516dbeabe68130b7
xiangyang.yang <xiangyang.yang@hdtmedia.com>
2019-10-18 10:28:00 +0800

    项目满足http和https协议

--- src/main/java/com/yang/DialogueProjectApplication.java
+++ src/main/java/com/yang/DialogueProjectApplication.java
@@ -5,6 +5,7 @@ import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,11 +18,28 @@ import org.springframework.context.annotation.Bean;
 @MapperScan("com.yang.dao")
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class DialogueProjectApplication {
+    @Value("${http.port}")
+    private Integer port;
+
+
+    // 配置http
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(port);
+        return connector;
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector()); // 添加http
+        return tomcat;
+    }
 
     public static void main(String[] args) {
 
         SpringApplication.run(DialogueProjectApplication.class, args);
     }
- 
+
 }
 
